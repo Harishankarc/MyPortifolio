d0b60522e0314f46bfc5b9e8faffb99fe21b5ef7
HARI SHANKAR <charishankar30@gmail.com>
2025-05-05 03:35:28 +0530

    changed some ui and projects

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -8,9 +8,9 @@ export default function Contact() {
                 Contact
             </h1>
             <div className="flex flex-col items-center gap-5 md:pb-0 pb-32">
-                <input type="text" className="border border-gray-500 rounded px-4 py-2 w-80 text-sm outline-none focus:ring-0 text-white" placeholder="Your Name" />
-                <input type="text" className="border border-gray-500 rounded px-4 py-2 w-80 text-sm outline-none focus:ring-0 text-white" placeholder="Your Email" />
-                <input type="text" className="border border-gray-500 rounded px-4 py-2 w-80 text-sm outline-none focus:ring-0 text-white" placeholder="Your Message" />
+                <input type="text" className="border border-gray-500 rounded px-4 py-2 w-80 text-sm outline-none focus:ring-0 text-white bg-transparent" placeholder="Your Name" />
+                <input type="text" className="border border-gray-500 rounded px-4 py-2 w-80 text-sm outline-none focus:ring-0 text-white bg-transparent" placeholder="Your Email" />
+                <input type="text" className="border border-gray-500 rounded px-4 py-2 w-80 text-sm outline-none focus:ring-0 text-white bg-transparent" placeholder="Your Message" />
                 <button className="text-[#64ffda] md:px-5 py-2 w-full rounded-md bg-transparent border border-[#64ffda] text-sm hover:bg-[#64ffda] hover:text-black">Submit</button>
             </div>
         </div>
